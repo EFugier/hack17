911fd6729983ff351e2bc5838bd539214dc46d5f
LoloSh <loic.fotso@yahoo.fr>
2017-11-25 23:13:12 +0100

    Add mock data and correct model

---
+++ src/mock-data/mock-users.ts
@@ -0,0 +1,27 @@
+import { User } from "../model/user";
+
+export const USERS: User[] = [
+  {
+    name: "Bob Dupont",
+    picture: '',
+    distance: 121456,
+    wallet: null,
+    drives: null
+  },
+
+  {
+    name: "Alice Durant",
+    picture: '',
+    distance: 646982,
+    wallet: null,
+    drives: null
+  },
+
+  {
+    name: "Charlie Roy",
+    picture: '',
+    distance: 9675243,
+    wallet: null,
+    drives: null
+  }
+]
